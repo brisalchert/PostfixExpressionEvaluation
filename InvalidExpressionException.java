2d0dcb22267b86036a48cf03484e98855e569f7e
bsalchert <salchert@hartford.edu>
2022-11-18 17:54:01 -0500

    Changed exception to be more general

--- InvalidExpressionException.java
+++ InvalidExpressionException.java
@@ -1,16 +1,15 @@
 //**********************************************************************
-//  InvalidNumberofIntegersException.java         Author: Brian Salchert
+//  InvalidExpressionException.java         Author: Brian Salchert
 //
 //  Extends the Exception class to provide an exception for instances
-//  with an invalid number of integers to properly evaluate an
-//  expression in PostfixExpression.java.
+//  with an invalid expression in PostfixExpression.java.
 //**********************************************************************
 
-public class InvalidNumberOfIntegersException extends Exception {
+public class InvalidExpressionException extends Exception {
     //------------------------------------------------------------------
     //  Prepares the error message for the exception.
     //------------------------------------------------------------------
-    InvalidNumberOfIntegersException (String message) {
+    InvalidExpressionException (String message) {
         super(message);
     }
 }
