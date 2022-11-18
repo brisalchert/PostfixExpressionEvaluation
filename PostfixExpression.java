9b4a561a9d168c4621e2d046f158ae181280866c
bsalchert <salchert@hartford.edu>
2022-11-18 17:34:06 -0500

    Added static method evaluate()

--- PostfixExpression.java
+++ PostfixExpression.java
@@ -1,3 +1,4 @@
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 //**********************************************************************
@@ -8,17 +9,65 @@ import java.util.Stack;
 //**********************************************************************
 
 public class PostfixExpression {
-    private Stack<Integer> operands = new Stack<Integer>();
-
-    public static int evaluate(String expression) throws TooManyIntegersException {
+    public static int evaluate(String expression) throws InvalidNumberOfIntegersException {
+        Stack<Integer> operands = new Stack<Integer>();
         String[] elements = expression.split(" ");
         int count;    // Ensures no more than 2 ints are entered before an operator
+        int result;
 
-        TooManyIntegersException problem =
-            new TooManyIntegersException("Too many integers in expression.");
+        InvalidNumberOfIntegersException problem =
+            new InvalidNumberOfIntegersException("Invalid number of integers in expression.");
 
         for (String element : elements) {
+            int nextNumber;
 
+            try {
+                nextNumber = Integer.parseInt(element);
+
+                operands.push(nextNumber);
+                count++;
+
+                if (count > 2) {
+                    throw problem;
+                }
+            }
+            catch (NumberFormatException exception) {
+                try {
+                    switch (element) {
+                        case "+":
+                            add();
+                            break;
+                        case "-":
+                            subtract();
+                            break;
+                        case "*":
+                            multiply();
+                            break;
+                        case "/":
+                            divide();
+                            break;
+                        case "%":
+                            modulo();
+                            break;
+                        default:
+                            System.out.println("Invalid operator in expression.");
+                    }
+                }
+                catch (EmptyStackException exception2) {
+                    throw problem;
+                }
+
+                count = 0;
+            }
         }
+
+        result = operands.pop();
+
+        if (!operands.empty()) {
+            throw problem;
+        }
+
+        return result;
+
     }
 }
