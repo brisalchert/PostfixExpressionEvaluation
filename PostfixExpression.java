e33addfb65eeea8cc9071e42baa5cecfc3c098f3
bsalchert <salchert@hartford.edu>
2022-11-18 17:20:54 -0500

    Added vars and exception for evaluate()

--- PostfixExpression.java
+++ PostfixExpression.java
@@ -9,4 +9,16 @@ import java.util.Stack;
 
 public class PostfixExpression {
     private Stack<Integer> operands = new Stack<Integer>();
+
+    public static int evaluate(String expression) throws TooManyIntegersException {
+        String[] elements = expression.split(" ");
+        int count;    // Ensures no more than 2 ints are entered before an operator
+
+        TooManyIntegersException problem =
+            new TooManyIntegersException("Too many integers in expression.");
+
+        for (String element : elements) {
+
+        }
+    }
 }
