9320bd167fbb52a75f2d63389e93f51c6a4ab448
bsalchert <salchert@hartford.edu>
2022-11-18 14:41:00 -0500

    Created and set up PostfixExpression.java

---
+++ PostfixExpression.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+//**********************************************************************
+//  PostfixExpression.java                        Author: Brian Salchert
+//
+//  Provides methods for evaluating an expression using postfix
+//  operators using the Stack class.
+//**********************************************************************
+
+public class PostfixExpression extends Stack {
+
+}
