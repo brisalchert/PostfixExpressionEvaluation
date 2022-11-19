ececda958b432bea87221d4eb5df0d533e2f935b
bsalchert <salchert@hartford.edu>
2022-11-19 16:01:18 -0500

    Added driver with user input for postfix expressions

--- PostfixExpressionEvaluation.java
+++ PostfixExpressionEvaluation.java
@@ -8,7 +8,24 @@
 import java.util.Scanner;
 
 public class PostfixExpressionEvaluation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidExpressionException {
+        Scanner input = new Scanner(System.in);
+        String userInput;
+        int calculationResult;
+        PostfixExpression expression = new PostfixExpression();
 
+        System.out.println();
+        System.out.print("\tEnter a postfix-operator expression: ");
+        userInput = input.nextLine();
+
+        calculationResult = expression.evaluate(userInput);
+
+        System.out.println();
+        System.out.print("\tEvaluated expression result: ");
+        System.out.print(calculationResult);
+        System.out.println();
+        System.out.println();
+
+        input.close();
     }
 }
